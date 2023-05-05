334f65a9206e74c53bf765c9a4ed056c4b45ffd2
Owen Himsworth <ohimsworth@olin.edu>
2023-05-05 11:51:15 -0400

    Remove test files for controller.py and view.py

--- test_aim_trainer_controller.py
+++
@@ -1,9 +0,0 @@
-"""
-Unit tests for aim trainer controller
-"""
-import pytest
-
-from aim_trainer_range import AimTrainerRange
-from aim_trainer_controller import AimTrainerController
-
-#Unit tests not added as they could not be implemented reasonably
--- test_aim_trainer_view.py
+++
@@ -1,9 +0,0 @@
-"""
-Unit test for aim trainer view
-"""
-import pytest
-
-from aim_trainer_range import AimTrainerRange
-from aim_trainer_view import AimTrainerView
-
-#Unit tests not added as they could not be implemented reasonably
